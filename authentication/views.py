0031beb2a10fdd30ac597643782aaafe71a39dd9
Daksh Pareek <dakshpareek7@gmail.com>
2019-09-14 12:51:39 +0530

    Authentication Added To Application

---
+++ authentication/views.py
@@ -0,0 +1,20 @@
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
+
+
+# Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.save()
+            raw_password = form.cleaned_data.get('password1')
+            #user = authenticate(username=user.username, passowrd=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
